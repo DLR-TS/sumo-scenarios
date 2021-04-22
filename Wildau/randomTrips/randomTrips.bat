2743a988911100b0da46157bbb6b75ebb4a47c76
Michael Behrisch <michael.behrisch@dlr.de>
2021-04-22 09:28:51 +0200

    adding sumocfg as example

--- Wildau/randomTrips/randomTrips.bat
+++ Wildau/randomTrips/randomTrips.bat
@@ -1,2 +1,1 @@
-
-"%SUMO_HOME%\tools\randomTrips.py"  -n ../Netzmodell2.net.xml --seed 42 --fringe-factor 7 -p 1.850 -r osm.passenger.routes.xml -b 53990 -e 61000 --vehicle-class passenger --vclass passenger --prefix veh --min-distance 300 --trip-attributes "departLane=\"best\"" --fringe-start-attributes "departSpeed=\"max\"" --allow-fringe.min-length 1000 --lanes  -L -l --validate
+"%SUMO_HOME%\tools\randomTrips.py"  -n ../Netzmodell2.net.xml --seed 42 --fringe-factor 7 -p 1.850 -r osm.passenger.rou.xml -b 53990 -e 61000 --vehicle-class passenger --vclass passenger --prefix veh --min-distance 300 --trip-attributes "departLane=\"best\"" --fringe-start-attributes "departSpeed=\"max\"" --allow-fringe.min-length 1000 --lanes --length --validate
