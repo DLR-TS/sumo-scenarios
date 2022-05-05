0b08cc8c8325d83992261140838fe341fd21cae9
Michael Behrisch <michael.behrisch@dlr.de>
2022-05-05 13:40:20 +0200

    adding gtfs import, fixing buslane connection

--- brunswick/miv/build.sh
+++ brunswick/miv/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 netconvert -c miv.netccfg
 netconvert -c miv2.netccfg
+if which python3; then
+    PYTHON=python3
+else
+    PYTHON=python
+fi
+$PYTHON $SUMO_HOME/tools/import/gtfs/gtfs2pt.py -n miv.net.xml.gz --gtfs ../gtfs/gtfs_connect-with_low_level_stops_20200514.zip --date 20200514 --begin 97200 --end 172800
 duarouter -c fromgeo.duarcfg
