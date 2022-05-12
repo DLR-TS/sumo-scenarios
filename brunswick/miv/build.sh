00de29b34ac0cb5cf5b2945ac1e03dbb247df32a
Michael Behrisch <michael.behrisch@dlr.de>
2022-05-12 16:49:26 +0200

    adding actuated control for Rudolfplatz

--- brunswick/miv/build.sh
+++ brunswick/miv/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-netconvert -c miv.netccfg
-netconvert -c miv2.netccfg
 if which python3 &> /dev/null; then
     PYTHON=python3
 else
     PYTHON=python
 fi
+#$PYTHON $SUMO_HOME/tools/tls/tls_csvSignalGroups.py -n miv.net.xml.gz -i netpatch/Rudolfplatz.csv -o netpatch/Rudolfplatz.tll.xml
+$PYTHON $SUMO_HOME/tools/tls/tls_csvSignalGroups.py -n miv.net.xml.gz -i netpatch/Rudolfplatz_SP33.csv -o netpatch/Rudolfplatz.tll.xml
+netconvert -c miv.netccfg
+netconvert -c miv2.netccfg
 $PYTHON $SUMO_HOME/tools/import/gtfs/gtfs2pt.py -n miv.net.xml.gz --gtfs ../gtfs/gtfs_connect-with_low_level_stops_20200514.zip --date 20200514 --begin 97200 --end 172800
 duarouter -c fromgeo.duarcfg
