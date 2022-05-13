10a9d85672e604decfe475d2673fc1b747004f9d
Michael Behrisch <oss@behrisch.de>
2022-05-13 19:29:42 +0200

    fixing build process

--- brunswick/miv/build.sh
+++ brunswick/miv/build.sh
@@ -4,9 +4,9 @@ if which python3 &> /dev/null; then
 else
     PYTHON=python
 fi
-#$PYTHON $SUMO_HOME/tools/tls/tls_csvSignalGroups.py -n miv.net.xml.gz -i netpatch/Rudolfplatz.csv -o netpatch/Rudolfplatz.tll.xml
-$PYTHON $SUMO_HOME/tools/tls/tls_csvSignalGroups.py -n miv.net.xml.gz -i netpatch/Rudolfplatz_SP33.csv -o netpatch/Rudolfplatz.tll.xml
 netconvert -c miv.netccfg
+#$PYTHON $SUMO_HOME/tools/tls/tls_csvSignalGroups.py -n netpatch/miv.net.xml -i netpatch/Rudolfplatz.csv -o netpatch/Rudolfplatz.tll.xml
+$PYTHON $SUMO_HOME/tools/tls/tls_csvSignalGroups.py -n netpatch/miv.net.xml -i netpatch/Rudolfplatz_SP33.csv -o netpatch/Rudolfplatz.tll.xml
 netconvert -c miv2.netccfg
 $PYTHON $SUMO_HOME/tools/import/gtfs/gtfs2pt.py -n miv.net.xml.gz --gtfs ../gtfs/gtfs_connect-with_low_level_stops_20200514.zip --date 20200514 --begin 97200 --end 172800
 duarouter -c fromgeo.duarcfg
