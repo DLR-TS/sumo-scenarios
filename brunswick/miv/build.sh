41f03bd6ed2a9cabb67d4f8a9e539c637f34d2a4
Michael Behrisch <oss@behrisch.de>
2023-03-05 21:18:07 +0100

    enabling pt build

--- brunswick/miv/build.sh
+++ brunswick/miv/build.sh
@@ -9,7 +9,5 @@ netconvert -c miv.netccfg
 #$PYTHON $SUMO_HOME/tools/tls/tls_csvSignalGroups.py -n netpatch/miv.net.xml -i netpatch/Rudolfplatz.csv -o netpatch/Rudolfplatz.tll.xml
 $PYTHON $SUMO_HOME/tools/tls/tls_csvSignalGroups.py -n netpatch/miv.net.xml -i netpatch/Rudolfplatz_SP33.csv -o netpatch/Rudolfplatz.tll.xml
 netconvert -c miv2.netccfg
-if test $# -ge 1; then
-    $PYTHON $SUMO_HOME/tools/import/gtfs/gtfs2pt.py -n miv.net.xml.gz --gtfs ../gtfs/gtfs_connect-with_low_level_stops_20200514.zip --date 20200514 --begin 97200 --end 172800
-fi
+$PYTHON $SUMO_HOME/tools/import/gtfs/gtfs2pt.py -n miv.net.xml.gz --gtfs ../gtfs/gtfs_connect-with_low_level_stops_20200514.zip --date 20200514 --begin 97200 --end 172800 --vtype-output ""
 duarouter -c fromgeo.duarcfg
