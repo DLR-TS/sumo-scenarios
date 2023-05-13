ec05b20dcb70c4cf1aaed9d7e55ce57b26689171
Michael Behrisch <oss@behrisch.de>
2023-05-13 19:13:24 +0200

    adding gtfs config and refactoring vtypes

--- brunswick/miv/build.sh
+++ brunswick/miv/build.sh
@@ -9,5 +9,5 @@ netconvert -c miv.netccfg
 #$PYTHON $SUMO_HOME/tools/tls/tls_csvSignalGroups.py -n netpatch/miv.net.xml -i netpatch/Rudolfplatz.csv -o netpatch/Rudolfplatz.tll.xml
 $PYTHON $SUMO_HOME/tools/tls/tls_csvSignalGroups.py -n netpatch/miv.net.xml -i netpatch/Rudolfplatz_SP33.csv -o netpatch/Rudolfplatz.tll.xml
 netconvert -c miv2.netccfg
-$PYTHON $SUMO_HOME/tools/import/gtfs/gtfs2pt.py -n miv.net.xml.gz --gtfs ../gtfs/gtfs_connect-with_low_level_stops_20200514.zip --date 20200514 --begin 97200 --end 172800 --vtype-output ""
+$PYTHON $SUMO_HOME/tools/import/gtfs/gtfs2pt.py -n miv.net.xml.gz --gtfs ../gtfs/gtfs_connect-with_low_level_stops_20200514.zip --date 20200514 --begin 97200 --end 172800 --vtype-output "" --skip-access
 duarouter -c fromgeo.duarcfg
