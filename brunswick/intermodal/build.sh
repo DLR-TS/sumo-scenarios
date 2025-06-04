758733e77afccbf2f489a706b38ec71913d05501
Jakob Erdmann <jakob.erdmann@dlr.de>
2025-06-04 13:53:21 +0200

    fixing intermodal demand build process

--- brunswick/intermodal/build.sh
+++ brunswick/intermodal/build.sh
@@ -2,3 +2,5 @@
 netconvert -c intermodal_pre.netccfg
 netconvert -c intermodal.netccfg
 duarouter -c fromgeo.duarcfg
+$PYTHON $SUMO_HOME/tools/import/gtfs/gtfs2pt.py -n intermodal.net.xml.gz --gtfs ../gtfs/gtfs_connect-with_low_level_stops_20200514.zip --date 20200514 --begin 97200 --end 172800 --vtype-output "" --skip-access --stops intermodal_stops.add.xml
+
