c0b1f998c32315a1551db380e1cc6b9e579d84d3
Jakob Erdmann <jakob.erdmann@dlr.de>
2025-06-04 13:29:55 +0200

    speeding up gtfs import

--- brunswick/miv/build.bat
+++ brunswick/miv/build.bat
@@ -2,5 +2,5 @@
 %SUMO_HOME%\bin\netconvert.exe -c miv.netccfg
 python %SUMO_HOME%\tools\tls\tls_csvSignalGroups.py -n netpatch/miv.net.xml -i netpatch/Rudolfplatz_SP33.csv -o netpatch/Rudolfplatz.tll.xml
 %SUMO_HOME%\bin\netconvert.exe -c miv2.netccfg
-python %SUMO_HOME%\tools\import\gtfs\gtfs2pt.py -n miv.net.xml.gz --gtfs ../gtfs/gtfs_connect-with_low_level_stops_20200514.zip --date 20200514 --begin 97200 --end 172800 --vtype-output "" --skip-access
+python %SUMO_HOME%\tools\import\gtfs\gtfs2pt.py -n miv.net.xml.gz --gtfs ../gtfs/gtfs_connect-with_low_level_stops_20200514.zip --date 20200514 --begin 97200 --end 172800 --vtype-output "" --skip-access --stops netpatch/miv_stops.add.xml
 %SUMO_HOME%\bin\duarouter.exe -c fromgeo.duarcfg
